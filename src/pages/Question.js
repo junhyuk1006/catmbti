41fd553528fcc2ac25cd01464921d243a2344cd9
jun <whdid3766@gmail.com>
2023-12-17 01:11:56 +0900

    add home page

---
+++ src/pages/Question.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Question = () =>{
+    <div>질문화면입니다.</div>
+}
+
+export default Question;
---
+++ src/pages/Result.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Result = () =>{
+    <div>결과화면입니다.</div>
+}
+
+export default Result;
