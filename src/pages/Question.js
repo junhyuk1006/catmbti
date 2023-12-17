2a13eeae9170189a3bd48d63feb66ee17a74fb8e
jun <whdid3766@gmail.com>
2023-12-17 15:36:14 +0900

    add question page

--- src/pages/Question.js
+++ src/pages/Question.js
@@ -1,9 +1,38 @@
 import React from "react";
+import { Button, ProgressBar } from "react-bootstrap";
+import styled from "styled-components";
+import {QuestionData} from '../assets/data/questiondata'
 
 const Question = () =>{
     return(
-        <div>질문화면입니다.</div>
+        <Wrapper>
+            <ProgressBar striped variant="danger" now={80} style={{marginTop: '20px'}}/>
+            <Title>{QuestionData[0].title}</Title>
+            <ButtonGroup>
+                <Button style={{width:'40%',minHeight: "200px", fontSize:"15pt"}}>{QuestionData[0].answera}</Button>
+                <Button style={{width:'40%',minHeight: "200px", fontSize:"15pt",marginLeft:"20px"}}>{QuestionData[0].answerb}</Button>
+            </ButtonGroup>
+        </Wrapper>
     );
 }
 
 export default Question;
+
+const Wrapper = styled.div`
+    height: 100vh;
+    width: 100%;
+`
+
+const Title =  styled.div`
+    font-size: 30pt;
+    text-align: center;
+    font-family:"DNFForgedBlade-Medium";
+`
+
+const ButtonGroup = styled.div`
+    display: flex;
+    flex-dirction: row;
+    align-items: center;
+    justify-content: center;
+    font-family:"DNFForgedBlade-Medium";
+`
